e7d61aedb40f711446cb23a3cad1f1a7e2e75156
Vaibhav Verma <148568999+Vaibhav-Verma21@users.noreply.github.com>
2026-07-26 02:23:46 +0530

    Time: 0 ms (100%), Space: 9.6 MB (5.95%) - LeetHub

---
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int maxProduct(int n) {
+    string s = to_string(n);
+    ranges::sort(s);
+    const int sz = s.length();
+    return (s[sz - 1] - '0') * (s[sz - 2] - '0');
+
+    }
+};

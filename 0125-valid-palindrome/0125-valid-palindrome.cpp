70ca2af90d517edb8ea59bdb62d4b7f76be2f537
Vaibhav Verma <148568999+Vaibhav-Verma21@users.noreply.github.com>
2026-08-08 14:42:33 +0530

    Time: 0 ms (100%), Space: 10 MB (36.52%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool helper(string& s, int l, int r){
+        if(l>=r)return true;
+        if(!isalnum(s[l])){
+            return helper(s,l+1,r);
+        }
+        if(!isalnum(s[r])){
+            return helper(s,l,r-1);
+        }
+        if(tolower(s[l]) != tolower(s[r])){
+            return false;
+        }
+        return helper(s,l+1,r-1);
+    }
+    bool isPalindrome(string s) {
+        return helper(s,0,s.length()-1);
+        
+    }
+};
